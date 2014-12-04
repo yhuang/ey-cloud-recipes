13ec10a6e4814acdc4912aa9e3b250f76be2f1c0
Jimmy Huang <jhuang@bleacherreport.com>
2014-12-04 14:30:43 -0800

    cannot do multiple commands as i had

--- cookbooks/binlog_purge_config/recipes/default.rb
+++ cookbooks/binlog_purge_config/recipes/default.rb
@@ -1,6 +1,7 @@
 if node[:instance_role] == 'db_slave'
   execute "keep binlog for 7 days" do
-    command "sed 's/keep_binlog_hours.*/keep_binlog_hours: 168/' < /etc/engineyard/binlogpurge.yml > /etc/engineyard/binlogpurge.yml.new"
-    command "mv /etc/engineyard/binlogpurge.yml.new /etc/engineyard/binlogpurge.yml"
+    binlogpurge_yml = "/etc/engineyard/binlogpurge.yml"
+    new_binlogpurge_yml = "/etc/engineyard/binlogpurge.yml.new"
+    command "sed 's/keep_binlog_hours.*/keep_binlog_hours: 168/' < #{binlogpurge_yml} > #{new_binlogpurge_yml}; mv #{new_binlogpurge_yml} #{binlogpurge_yml}"
   end
 end
