0e6fbebbd7acef23dafe0cbd9694054617d638ae
Jimmy Huang <jhuang@bleacherreport.com>
2014-12-04 14:17:34 -0800

    fixed typo in mv command

--- cookbooks/binlog_purge_config/recipes/default.rb
+++ cookbooks/binlog_purge_config/recipes/default.rb
@@ -1,6 +1,6 @@
 if node[:instance_role] == 'db_slave'
   execute "keep binlog for 7 days" do
     command "sed 's/keep_binlog_hours.*/keep_binlog_hours: 168/' < /etc/engineyard/binlogpurge.yml > /etc/engineyard/binlogpurge.yml.new"
-    command "mv /etc/engineyard/binlogpurge.yml.new > /etc/engineyard/binlogpurge.yml"
+    command "mv /etc/engineyard/binlogpurge.yml.new /etc/engineyard/binlogpurge.yml"
   end
 end
